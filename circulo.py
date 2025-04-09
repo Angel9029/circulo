64b178e0a569021bda58ea00dd40f55c2809588a
Angel Perez Ravelo <75326952@continental.edu.pe>
2025-04-09 11:36:39 -0500

    Implementando area

--- circulo.py
+++ circulo.py
@@ -3,6 +3,8 @@ import math
 """
 Construcctor del objeto Circulo
 """
+
+
 class Circulo:
     def __init__(self, radio):
         self.radio = radio
@@ -10,6 +12,11 @@ class Circulo:
     def circunferencia(self):
         return 2 * math.pi * self.radio
 
+    def area(self):
+        return math.pi * self.radio ** 2
+
+
 if __name__ == "__main__":
     instancia_circulo = Circulo(10)
     print(f"La circunferencia es: {instancia_circulo.circunferencia()}")
+    print(f"El area es: {instancia_circulo.area()}")
