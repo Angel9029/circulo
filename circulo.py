df32dd3b478974d1171f22446e0d69f225594674
Angel Perez Ravelo <75326952@continental.edu.pe>
2025-04-09 11:18:19 -0500

    Implementacion de circunferencia

--- circulo.py
+++ circulo.py
@@ -1,2 +1,15 @@
+import math
+
+"""
+Construcctor del objeto Circulo
+"""
 class Circulo:
-    pass
+    def __init__(self, radio):
+        self.radio = radio
+
+    def circunferencia(self):
+        return 2 * math.pi * self.radio
+
+if __name__ == "__main__":
+    instancia_circulo = Circulo(10)
+    print(f"La circunferencia es: {instancia_circulo.circunferencia()}")
